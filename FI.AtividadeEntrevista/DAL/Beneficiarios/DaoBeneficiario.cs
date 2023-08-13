c0593a0a296388ba0cd4d283fd78b25fcf94f721
Gabriel Fernandes Giraud <gabrielgiraud71@gmail.com>
2023-08-12 21:35:49 -0300

    feat(back): starting add dal beneficiarios

---
+++ FI.AtividadeEntrevista/DAL/Beneficiarios/DaoBeneficiario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FI.AtividadeEntrevista.DAL.Beneficiarios
+{
+    /// <summary>
+    /// Classe de acesso a dados de Beneficiario
+    /// </summary>
+    internal class DaoBeneficiario : AcessoDados
+    {
+    }
+}
---
+++ FI.AtividadeEntrevista/DAL/Beneficiarios/Procedures/BeneficiarioProcedureEnum.cs
@@ -0,0 +1,14 @@
+﻿namespace FI.AtividadeEntrevista.DAL.Clientes.Procedures
+{
+    /// <summary>
+    /// Enum de procedures disponíveis para o modelo Beneficiario
+    /// </summary>
+    public static class BeneficiarioProcedureEnum
+    {
+        public static string Alterar = "FI_SP_AltBeneficiario";
+        public static string Incluir = "FI_SP_IncBeneficiario";
+        public static string Excluir = "FI_SP_DelBeneficiario";
+        public static string Pesquisar = "FI_SP_PesqBeneficiario";
+        public static string ExisteCPF = "FI_SP_VerificaBeneficiario";
+    }
+}
